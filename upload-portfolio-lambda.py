8fbf2a90ed9ba531d4838646360bd47bc9d3db32
Jitin <kjitin@gmail.com>
2019-06-20 22:16:39 +0100

    Modified Lambda function

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,20 +1,28 @@
+import json
 import boto3
 from botocore.client import Config
 from io import BytesIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3',config=Config(signature_version='s3v4'))
-portfolio_bucket = s3.Bucket('portfolio.jitinkayyala.co.uk')
 
-portfolio_bucket.download_file('index.html','/tmp/index.html')
-build_bucket = s3.Bucket('portfoliobuild.jitinkayyala.co.uk')
-portfolio_zip = BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:956118533541:deployPortfolioTopic')
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    portfolio_bucket = s3.Bucket('portfolio.jitinkayyala.co.uk')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj,nm,
-        ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    portfolio_bucket.download_file('index.html', '/tmp/index.html')
+    build_bucket = s3.Bucket('portfoliobuild.jitinkayyala.co.uk')
+    portfolio_zip = BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    print('Job Done')
+    topic.publish(Subject='Portfolio deployed',Message='Portfolio deployed successfully')
+    return 'Hello from Lambda'
